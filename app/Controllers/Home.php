ab4b6e9594ef01526ebd57b65d2a42c892230db4
Arya Yunanta <aryayun90@gmail.com>
2021-07-07 09:31:35 +0700

    Fix footer top bug

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,12 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-	public function index()
-	{
-		return view('pages/LandingPage');
-	}
+    public function index()
+    {
+        $data = [
+            'title' => 'SRIFOTON 2021',
+            'footer_top' => '200vh'
+        ];
+        return view('pages/LandingPage', $data);
+    }
 }
