94913770fff4ff38d563d39e33d32ce83f01a056
Irvan Malik <irvanmalik48@gmail.com>
2021-08-25 20:21:25 +0700

    fix missing names in input
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- app/Views/pages/account_update.php
+++ app/Views/pages/account_update.php
@@ -12,20 +12,21 @@
         <div class="row">
             <div class="col-10 col-lg-6 mx-auto mt-5">
                 <p><?= $validation->listErrors() ?></p>
-                <form action="/dashboard/update/save" method="POST">
+                <form action="/dashboard/update/save" method="post">
                     <?= csrf_field(); ?>
                     <div class="mb-3">
-                        <label for="name" class="form-label">Name</label>
+                        <label for="full_name" class="form-label">Name</label>
                         <input
                                 type="text"
                                 class="form-control"
-                                id="name"
+                                id="full_name"
+                                name="full_name"
                                 value="<?= $full_name ?>"
                         />
                     </div>
                     <div class="mb-3">
                         <label for="nim" class="form-label">NIM</label>
-                        <input type="text" class="form-control" id="nim" value="<?= $nim ?>"/>
+                        <input type="text" class="form-control" id="nim" name="nim" value="<?= $nim ?>"/>
                     </div>
                     <div class="mb-3">
                         <label for="email" class="form-label">Email address</label>
@@ -33,19 +34,20 @@
                                 type="email"
                                 class="form-control"
                                 id="email"
+                                name="email"
                                 placeholder="name@example.com"
                                 value="<?= $email ?>"
                         />
                     </div>
                     <div class="mb-3">
                         <label for="university" class="form-label">Universitas</label>
-                        <input type="text" class="form-control" id="university" value="<?= $university ?>"/>
+                        <input type="text" class="form-control" id="university" name="university" value="<?= $university ?>"/>
                     </div>
                     <div class="mb-3">
-                        <label for="Whatsapp_num" class="form-label"
+                        <label for="whatsapp_num" class="form-label"
                         >Whatsapp Number</label
                         >
-                        <input type="text" class="form-control" id="Whatsapp_num" value="<?= $whatsapp_num ?>"/>
+                        <input type="text" class="form-control" id="whatsapp_num" name="whatsapp_num" value="<?= $whatsapp_num ?>"/>
                     </div>
                     <button type="submit" class="mt-3 btn btn-primary">Update</button>
                 </form>
